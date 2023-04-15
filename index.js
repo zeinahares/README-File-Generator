db59cb9feadb507eba05a07546e36350c1d9ea39
zeinahares <zeinahares@gmail.com>
2023-04-16 06:47:38 +1000

    added markdown and license functions

--- index.js
+++ index.js
@@ -1,131 +1,73 @@
 // Include packages needed for this application
 const inquirer = require('inquirer');
-const markdownMaker = require('./utils/generateMarkdown');
+const markdownMaker = require('./utils/generateMarkdown.js');
 const fs = require('fs');
 
-// XX - GIVEN a command-line application that accepts user input
-// WHEN I am prompted for information about my application repository
-// XX - THEN a high-quality, professional README.md is generated with the title of my project and 
-// XX - sections entitled Description, Table of Contents, Installation, Usage, License, Contributing, Tests, and Questions
-// WHEN I enter my project title
-// - THEN this is displayed as the title of the README
-// WHEN I enter a description, installation instructions, usage information, contribution guidelines, and test instructions
-// THEN this information is added to the sections of the README entitled Description, Installation, Usage, Contributing, and Tests
-// WHEN I choose a license for my application from a list of options
-// THEN a badge for that license is added near the top of the README and a notice is added to the section of the README entitled 
-// License that explains which license the application is covered under
-
-// WHEN I enter my GitHub username
-// THEN this is added to the section of the README entitled Questions, with a link to my GitHub profile
-// WHEN I enter my email address
-// THEN this is added to the section of the README entitled Questions, with instructions on how to reach me with additional questions
-// WHEN I click on the links in the Table of Contents
-// THEN I am taken to the corresponding section of the README
-
-
-// # <Your-Project-Title> - also file name
-
-// ## Description
-// Provide a short description explaining the what, why, and how of your project
-
-// ## Table of contents
-// WHEN I click on the links in the Table of Contents
-// THEN I am taken to the corresponding section of the README
-
-// ## Installation
-// What are the steps required to install your project? 
-
-// ## Usage
-// Provide instructions and examples for use. 
-
-// ## License
-// choices
-// add badge near the top of the page, as well as description
-
-// ## Contribution
-// List your collaborators, if any, with links to their GitHub profiles.
-// ## Tests
-// ## Questions - with instructions on how to reach me with additional questions
-
-// Github username -added to Questions
-// email address - added to questions
-// 
-
-
 // Create an array of questions for user input
-const questions = ["What's your project title?", 
-"Provide a short description explaining the what, why, and how of your project",
-"What are the steps required to install your project?", 
-"Provide instructions and examples for use. ",
-"Choose your License. If you need help, refer to (https://choosealicense.com/).",
-"List your collaborators, if any, with links to their GitHub profiles.",
-"Write tests for your application. Then provide examples on how to run them here.",
-"What's your Github username?",
-"What is your email address?",
-];
+const questions = [
+    {
+      type: 'input',
+      name: 'name',
+      message: "What's your project title?",
+    },
+    {
+      type: 'input',
+      message: "Provide a short description explaining the what, why, and how of your project",
+      name: 'description',
+    },
+    {
+      type: 'input',
+      message: "What are the steps required to install your project?",
+      name: 'installation',
+    },
+    {
+      type: 'input',
+      message: "Provide instructions and examples for use. ",
+      name: 'usage',
+    },
+    {
+      type: 'list',
+      message: "Choose your License. If you need help, refer to (https://choosealicense.com/).",
+      name: 'license',
+      choices: ['The Unlicense',
+      'Mozilla Public License 2.0',
+      'Apache License 2.0',
+      'MIT License',
+      'N/A'],
+    },
+    {
+      type: 'input',
+      message: "List your collaborators, if any, with links to their GitHub profiles.",
+      name: 'collab',
+    },
+    {
+      type: 'input',
+      message: "Write tests for your application. Then provide examples on how to run them here.",
+      name: 'test',
+    },
+    {
+      type: 'input',
+      message: "What's your Github username?",
+      name: 'github',
+    },
+    {
+      type: 'input',
+      message: "What is your email address?",
+      name: 'email',
+    },
+  ]
 
 // Create a function to write README file
 function writeToFile(fileName, data) {
 
-    let readmeContent = markdownMaker.generateMarkdown(data);
-
-    fs.writeFile(fileName, readmeContent, (err) =>
+    fs.writeFile(fileName, markdownMaker.generateMarkdown(data), (err) =>
       err ? console.log(err) : console.log('Successfully created index.html!')
     );
 }
 
 // Create a function to initialize app
 const init = (questions) => {
-    inquirer
-        .prompt([
-      {
-        type: 'input',
-        name: 'name',
-        message: questions[0],
-      },
-      {
-        type: 'input',
-        message: questions[1],
-        name: 'description',
-      },
-      {
-        type: 'input',
-        message: questions[2],
-        name: 'installation',
-      },
-      {
-        type: 'input',
-        message: questions[3],
-        name: 'usage',
-      },
-      {
-        type: 'list',
-        message: questions[4],
-        name: 'license',
-        choices: ['GNU AGPLv3', 'GNU GPLv3', 'GNU LGPLv3', 'Mozilla Public License 2.0',
-    'Apache License 2.0','MIT License','Boost Software License 1.0','The Unlicense'],
-      },
-      {
-        type: 'input',
-        message: questions[5],
-        name: 'collab',
-      },
-      {
-        type: 'input',
-        message: questions[6],
-        name: 'test',
-      },
-      {
-        type: 'input',
-        message: questions[7],
-        name: 'github',
-      },
-      {
-        type: 'input',
-        message: questions[8],
-        name: 'email',
-      },
-    ])
+    inquirer.prompt(questions)
     .then((data) => {
       const fileName = `${data.name.toUpperCase().split(' ').join('')}.md`;
   
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,21 +1,174 @@
-// TODO: Create a function that returns a license badge based on which license is passed in
+// Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+function renderLicenseBadge(license) {
 
-// TODO: Create a function that returns the license link
+  let licenseLink = renderLicenseLink(license);
+  if (license == 'The Unlicense') {
+    return `[![License: Unlicense](https://img.shields.io/badge/license-Unlicense-blue.svg)](${licenseLink})`
+  } else if (license == 'Mozilla Public License 2.0') {
+    return `[![License: MPL 2.0](https://img.shields.io/badge/License-MPL_2.0-brightgreen.svg)](${licenseLink})`;
+
+  } else if (license == 'Apache License 2.0') {
+    return `[![License: Apache License 2.0](https://img.shields.io/badge/License-Apache_2.0-blue.svg)](${licenseLink})`;
+
+  } else if (license == 'MIT License') {
+    return `[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](${licenseLink})`;
+
+  } else if (license == 'N/A') {
+    return '';
+
+  }
+}
+
+// function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {}
+function renderLicenseLink(license) {
 
-// TODO: Create a function that returns the license section of README
+  if (license == 'The Unlicense') {
+    return 'http://unlicense.org/';
+
+  } else if (license == 'Mozilla Public License 2.0') {
+    return 'https://opensource.org/licenses/MPL-2.0';
+
+  } else if (license == 'Apache License 2.0') {
+    return 'https://opensource.org/licenses/Apache-2.0';
+
+  } else if (license == 'MIT License') {
+    return 'https://opensource.org/licenses/MIT';
+
+  } else if (license == 'N/A') {
+    return '';
+
+  }
+}
+
+// Create a function that returns the license section of README
 // If there is no license, return an empty string
-function renderLicenseSection(license) {}
+function renderLicenseSection(license) {
+  let licenseLink = renderLicenseLink(license);
 
-// TODO: Create a function to generate markdown for README
+  if (license == 'The Unlicense') {
+    return `
+    The Unlicense (${licenseLink})
+
+
+    This is free and unencumbered software released into the public domain.
+
+    Anyone is free to copy, modify, publish, use, compile, sell, or
+    distribute this software, either in source code form or as a compiled
+    binary, for any purpose, commercial or non-commercial, and by any
+    means.
+    
+    In jurisdictions that recognize copyright laws, the author or authors
+    of this software dedicate any and all copyright interest in the
+    software to the public domain. We make this dedication for the benefit
+    of the public at large and to the detriment of our heirs and
+    successors. We intend this dedication to be an overt act of
+    relinquishment in perpetuity of all present and future rights to this
+    software under copyright law.
+    
+    THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+    EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+    MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+    IN NO EVENT SHALL THE AUTHORS BE LIABLE FOR ANY CLAIM, DAMAGES OR
+    OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE,
+    ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+    OTHER DEALINGS IN THE SOFTWARE.
+    
+    For more information, please refer to <${licenseLink}>`;
+
+  } else if (license == 'Mozilla Public License 2.0') {
+    return `Mozilla Public License 2.0 ${licenseLink}
+
+    This Source Code Form is subject to the terms of the Mozilla Public License, v. 2.0. If a copy of the MPL was not distributed with this file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
+    If it is not possible or desirable to put the notice in a particular file, then You may include the notice in a location (such as a LICENSE file in a relevant directory) where a recipient would be likely to look for such a notice.
+    
+    You may add additional accurate notices of copyright ownership.`;
+
+  } else if (license == 'Apache License 2.0') {
+    return `Apache License 2.0 ${licenseLink}
+
+    Copyright (c) [yyyy] [name of copyright owner]
+
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+
+      ${licenseLink}
+
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
+    `;
+
+  } else if (license == 'MIT License') {
+    return `!MIT License ${licenseLink}
+
+    Copyright (c) [yyyy] [name of copyright owner]
+    
+    Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+    
+    The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+    
+    THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.`;
+
+  } else if (license == 'N/A') {
+    return 'N/A';
+
+  }
+
+}
+
+// Create a function to generate markdown for README
 function generateMarkdown(data) {
-  
-  return `# ${data.title}
 
+  let licenseBody = renderLicenseSection(data.license);
+  let badge = renderLicenseBadge(data.license);
+
+  return `# ${data.name}
+
+${badge}
+
+## Description
+${data.description}
+
+## Table of contents
+- [Installation](#installation)
+- [Usage](#usage)
+- [License](#license)
+- [Contribution](#contribution)
+- [Tests](#tests)
+- [Questions](#questions)
+ 
+
+## Installation
+${data.installation}
+
+## Usage
+${data.usage} 
+
+## License
+${licenseBody}
+
+## Contribution
+${data.collab} 
+
+## Tests
+${data.test} 
+## Questions - 
+  
+Feel free to reach me for questions at anytime!
+
+  GitHub URL: https://github.com/${data.github} 
+
+
+  Email Address: ${data.email} 
 `;
 }
 
-module.exports = generateMarkdown;
+module.exports = {
+  generateMarkdown
+};
